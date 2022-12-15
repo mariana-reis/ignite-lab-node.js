ed5a8f2c90023e0f67f6043b7ed3a435ccb56a77
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-15 15:53:11 -0300

    :sparkles: feat: creation send notification

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,31 @@
+import { Content } from "../entities/content";
+import { Notification } from "../entities/notification";
+
+
+interface ISendNotificationRequest {
+  recipientId: string;
+  content: string
+  category: string
+}
+
+interface ISendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  async execute(request:ISendNotificationRequest): Promise<ISendNotificationResponse> {
+    const { recipientId, content, category} = request;
+    
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    })
+    
+    // persist notification on base de dados
+    
+    return {
+      notification
+    };
+  }
+}
