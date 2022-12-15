0d66d6cdb86236ea85ae1974b88c43a8c4d86d26
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-15 15:30:09 -0300

    :test_tube: creating notification test

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,4 @@
+import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
 export interface INotificationProps {
@@ -11,44 +12,47 @@ export interface INotificationProps {
 export class Notification {
   private props: INotificationProps;
   
-  constructor(props: INotificationProps) {
-    this.props = props;
+  constructor(props: Replace<INotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
   
   public set recipientId(recipientId: string) {
-    this.props.recipientId = recipientId
+    this.props.recipientId = recipientId;
   }
   
   public get recipientId(): string {
-    return this.props.recipientId
+    return this.props.recipientId;
   }
   
   public set content(content: Content) {
-    this.props.content = content
+    this.props.content = content;
   }
   
   public get content(): Content {
-    return this.props.content
+    return this.props.content;
   }
   
   public set category(category: string) {
-    this.props.category = category
+    this.props.category = category;
   }
   
   public get category(): string {
-    return this.props.category
+    return this.props.category;
   }
   
   public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt
+    this.props.readAt = readAt;
   }
   
   public get createdAt(): Date {
-    return this.props.createdAt
+    return this.props.createdAt;
   }
   
   public get readAt(): Date | null | undefined {
-    return this.props.readAt
+    return this.props.readAt;
   }
   
 }
