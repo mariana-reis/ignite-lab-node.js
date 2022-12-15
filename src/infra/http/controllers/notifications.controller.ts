43b2819076c57db9b9ebdea6c8e0a7c5a2e7a74c
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-15 20:28:32 -0300

    :zap: perf: creating view models

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,3 +1,4 @@
+import { NotificationViewModel } from './../../view-models/notification-view-model';
 import { SendNotification } from '@app/use-cases/send-notification';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
@@ -17,12 +18,7 @@ export class NotificationsController {
     });
     
     return { 
-      notification: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId
-      }
+      notification: NotificationViewModel.toHTTP(notification)
     };
   }
 }
