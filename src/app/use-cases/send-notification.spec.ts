3ca9fc0971105496e251e7c6fa8cdf28df9a81b8
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-15 17:14:48 -0300

    :recycle: refactor: refactoring and adding in-memory database

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,24 +1,19 @@
-import { Notification } from './../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository'
 import { SendNotification } from "./send-notification"
-const notifications: Notification[] = []
-// create repository fake
-const notificationsRepository = {
-    async create(notification: Notification) {
-      notifications.push(notification)
-    }
-}
+
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
     const sendNotification = new SendNotification(notificationsRepository)
     
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content:'this is a notification',
       category: 'social',
       recipientId: 'exemple-recipientId'
     })
     
-    console.log(notifications)
-    expect(notifications).toHaveLength(1)
+    expect(notificationsRepository.notifications).toHaveLength(1)
+    expect(notificationsRepository.notifications[0]).toEqual(notification)
   })
 })
