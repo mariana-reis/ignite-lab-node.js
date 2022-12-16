88acc88bf2fd7a94f885189d93f9cfd296830c13
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-16 11:42:43 -0300

    :boom: fix: prisma notification repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,5 +1,6 @@
+import { Content } from './../../../../app/entities/content';
 import { Notification } from '@app/entities/notification';
-
+import { Notification as RawNotification } from '@prisma/client'
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
     return {
@@ -11,4 +12,19 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt
     }
   }
+  
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        cancelAt: raw.cancelAt,
+        createdAt: raw.createdAt
+      }, 
+      raw.id
+    ); 
+  }
+  
 }
