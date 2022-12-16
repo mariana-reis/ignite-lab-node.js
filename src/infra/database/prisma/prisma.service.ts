2591ce2ee09151cb5ef46d35d3bdeacb091aa23e
mariana-reis <mariana.reis@al.resilia.com.br>
2022-12-16 12:40:00 -0300

    :sparkles: feat: adding HTTP routes in controller

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -3,6 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  //show the query logs in the database
+  // constructor() {
+  //   super({
+  //     log: ['query']
+  //   });
+  // }
+  
   async onModuleInit() {
     await this.$connect();
   }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,13 @@
+import { CancelNotification } from '@app/use-cases/cancel-notification';
 import { SendNotification } from '@app/use-cases/send-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module';
+
 
 @Module({
   imports:[DatabaseModule],
@@ -9,7 +15,12 @@ import { DatabaseModule } from '../database/database.module';
     NotificationsController
   ],
   providers:[
-    SendNotification
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification
   ]
 })
 
